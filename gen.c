3f4437ed32fb5e52226fdf1a9bf4f73290ee002d
hirrolot <hirrolot@gmail.com>
2021-06-29 16:00:09 +0600

    Sort the posts by date

--- gen.c
+++ gen.c
@@ -70,7 +70,33 @@
     "Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white\" "           \
     "/></a>"
 
-static void collect_post_names(size_t *restrict posts_count,
+typedef enum {
+    Jan = 1,
+    Feb,
+    Mar,
+    Apr,
+    May,
+    Jun,
+    Jul,
+    Aug,
+    Sept,
+    Oct,
+    Nov,
+    Dec,
+} Month;
+
+typedef struct {
+    unsigned day;
+    Month month;
+    unsigned year;
+} PostDate;
+
+typedef struct {
+    char *title;
+    PostDate date;
+} PostMetadata;
+
+static void collect_post_names(size_t *posts_count,
                                char *post_names[POSTS_MAX]);
 
 static void gen_index_md(size_t posts_count,
@@ -82,6 +108,26 @@ static void gen_target_index(FILE *makefile);
 static void gen_phony_all(FILE *makefile, size_t posts_count,
                           const char *post_names[static posts_count]);
 static void gen_phony_clean(FILE *makefile);
+
+static PostMetadata *
+PostMetadata_collect_all(size_t posts_count,
+                         const char *post_names[static posts_count]);
+
+static unsigned
+PostMetadata_min_year(size_t posts_count,
+                      PostMetadata metadata[static posts_count]);
+static unsigned
+PostMetadata_max_year(size_t posts_count,
+                      PostMetadata metadata[static posts_count]);
+
+static Month Month_parse(const char *str);
+static const char *Month_str(Month self);
+
+static PostDate PostDate_parse(const char *str);
+static char *PostDate_str(PostDate self);
+
+static PostMetadata PostMetadata_parse(const char *str);
+static void PostMetadata_free(PostMetadata *self);
 
 static char *find_post_title(const char *str);
 static char *find_post_date(const char *str);
@@ -116,7 +162,7 @@ int main(void) {
     assert(makefile_closed);
 }
 
-static void collect_post_names(size_t *restrict posts_count,
+static void collect_post_names(size_t *posts_count,
                                char *post_names[POSTS_MAX]) {
     DIR *dir = opendir(CONTENT_DIR);
     assert(dir);
@@ -154,36 +200,42 @@ static void gen_index_md(size_t posts_count,
                    "---\n\n");
     fprintf(index, "%s", EXCELLENT_ME);
 
-    for (size_t i = 0; i < posts_count; i++) {
-        if (strcmp(post_names[i], "index") == 0) {
-            continue;
+    PostMetadata *metadata = PostMetadata_collect_all(posts_count, post_names);
+
+    const unsigned min_year = PostMetadata_min_year(posts_count, metadata),
+                   max_year = PostMetadata_max_year(posts_count, metadata);
+
+    for (unsigned year = max_year; year >= min_year; year--) {
+        for (Month month = Dec; month >= Jan; month--) {
+            for (unsigned day = 31; day >= 1; day--) {
+                for (size_t i = 0; i < posts_count; i++) {
+                    if (strcmp(metadata[i].title, "index") == 0) {
+                        continue;
+                    }
+
+                    if (metadata[i].date.year != year ||
+                        metadata[i].date.month != month ||
+                        metadata[i].date.day != day) {
+                        continue;
+                    }
+
+                    char *date = PostDate_str(metadata[i].date);
+
+                    fprintf(index,
+                            " - [%s](" OUTPUT_DIR "/%s.html)<br>%s<br>\n",
+                            metadata[i].title, post_names[i], date);
+
+                    free(date);
+                }
+            }
         }
-
-        char post_path[128] = {0};
-        snprintf(post_path, sizeof post_path, CONTENT_DIR "/%s.md",
-                 post_names[i]);
-
-        FILE *post_fp = fopen(post_path, "r");
-        assert(post_fp);
-
-        char post_beginning[512] = {0};
-        const size_t chars_read =
-            fread(post_beginning, sizeof post_beginning[0],
-                  sizeof post_beginning - 1, post_fp);
-        assert(sizeof post_beginning - 1 == chars_read);
-
-        char *title = find_post_title(post_beginning),
-             *date = find_post_date(post_beginning);
-
-        fprintf(index, " - [%s](" OUTPUT_DIR "/%s.html)<br>%s<br>\n", title,
-                post_names[i], date);
-
-        free(title);
-        free(date);
-
-        const bool post_fp_closed = fclose(post_fp) == 0;
-        assert(post_fp_closed);
     }
+
+    for (size_t i = 0; i < posts_count; i++) {
+        PostMetadata_free(&metadata[i]);
+    }
+
+    free(metadata);
 
     const bool index_closed = fclose(index) == 0;
     assert(index_closed);
@@ -218,6 +270,151 @@ static void gen_phony_all(FILE *makefile, size_t posts_count,
 static void gen_phony_clean(FILE *makefile) {
     fprintf(makefile, "clean:\n\trm " OUTPUT_DIR "/*.html\n\n");
 }
+
+static PostMetadata *
+PostMetadata_collect_all(size_t posts_count,
+                         const char *post_names[static posts_count]) {
+    PostMetadata *metadata = malloc(sizeof metadata[0] * posts_count);
+    assert(metadata);
+
+    for (size_t i = 0; i < posts_count; i++) {
+        char post_path[128] = {0};
+        snprintf(post_path, sizeof post_path, CONTENT_DIR "/%s.md",
+                 post_names[i]);
+
+        FILE *post_fp = fopen(post_path, "r");
+        assert(post_fp);
+
+        char post_beginning[512] = {0};
+        const size_t chars_read =
+            fread(post_beginning, sizeof post_beginning[0],
+                  sizeof post_beginning - 1, post_fp);
+        assert(sizeof post_beginning - 1 == chars_read);
+
+        metadata[i] = PostMetadata_parse(post_beginning);
+
+        const bool post_fp_closed = fclose(post_fp) == 0;
+        assert(post_fp_closed);
+    }
+
+    return metadata;
+}
+
+static unsigned
+PostMetadata_min_year(size_t posts_count,
+                      PostMetadata metadata[static posts_count]) {
+    unsigned min_year = 9999;
+
+    for (size_t i = 0; i < posts_count; i++) {
+        if (strcmp(metadata[i].title, "index") == 0) {
+            continue;
+        }
+
+        if (min_year > metadata[i].date.year) {
+            min_year = metadata[i].date.year;
+        }
+    }
+
+    return min_year;
+}
+
+static unsigned
+PostMetadata_max_year(size_t posts_count,
+                      PostMetadata metadata[static posts_count]) {
+    unsigned max_year = 0;
+
+    for (size_t i = 0; i < posts_count; i++) {
+        if (strcmp(metadata[i].title, "index") == 0) {
+            continue;
+        }
+
+        if (max_year < metadata[i].date.year) {
+            max_year = metadata[i].date.year;
+        }
+    }
+
+    return max_year;
+}
+
+#define MONTHS                                                                 \
+    ASSOC(Jan);                                                                \
+    ASSOC(Feb);                                                                \
+    ASSOC(Mar);                                                                \
+    ASSOC(Apr);                                                                \
+    ASSOC(May);                                                                \
+    ASSOC(Jun);                                                                \
+    ASSOC(Jul);                                                                \
+    ASSOC(Aug);                                                                \
+    ASSOC(Sept);                                                               \
+    ASSOC(Oct);                                                                \
+    ASSOC(Nov);                                                                \
+    ASSOC(Dec)
+
+static Month Month_parse(const char *str) {
+#define ASSOC(month)                                                           \
+    do {                                                                       \
+        if (strcmp(str, #month) == 0) {                                        \
+            return month;                                                      \
+        }                                                                      \
+    } while (0)
+
+    MONTHS;
+
+    assert(false);
+    return 0;
+
+#undef ASSOC
+}
+
+static const char *Month_str(Month self) {
+#define ASSOC(month)                                                           \
+    case month:                                                                \
+        return #month;
+
+    switch (self) {
+        MONTHS;
+    default:
+        assert(false);
+        return NULL;
+    }
+
+#undef ASSOC
+}
+
+#undef MONTHS
+
+static PostDate PostDate_parse(const char *str) {
+    PostDate self;
+
+    char *date = find_post_date(str);
+
+    char month[16];
+    const int items_read =
+        sscanf(date, "%s %u, %u", month, &self.day, &self.year);
+    assert(3 == items_read);
+
+    self.month = Month_parse(month);
+
+    free(date);
+
+    return self;
+}
+
+static char *PostDate_str(PostDate self) {
+    char result[16];
+    snprintf(result, sizeof result, "%s %u, %u", Month_str(self.month),
+             self.day, self.year);
+    return strdup(result);
+}
+
+static PostMetadata PostMetadata_parse(const char *str) {
+    return (PostMetadata){
+        .title = find_post_title(str),
+        .date = PostDate_parse(str),
+    };
+}
+
+static void PostMetadata_free(PostMetadata *self) { free(self->title); }
 
 static char *find_post_title(const char *str) {
     char *title_start = strstr(str, "title: \"");
