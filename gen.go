0e7f4b3da88206fbfc407f732d09576ed8464f3c
hirrolot <hirrolot@gmail.com>
2022-10-19 17:44:38 +0600

    Automate the creation of new posts

--- gen.go
+++ gen.go
@@ -19,9 +19,10 @@ const (
 )
 
 type Post struct {
-	Name, Title string // Always present.
-	Date        string // Always present.
-	RedirectTo  string `json:"redirect-to"` // "" in case of a regular post.
+	Name       string `json:"name"`                  // Always present.
+	Title      string `json:"title"`                 // Always present.
+	Date       string `json:"date"`                  // Always present.
+	RedirectTo string `json:"redirect-to,omitempty"` // "" in case of a regular post.
 }
 
 func (post *Post) inputFilename() string {
@@ -42,6 +43,11 @@ func (post *Post) parseDate() time.Time {
 }
 
 func main() {
+	if len(os.Args) == 2 && os.Args[1] == "new" {
+		newPost()
+		return
+	}
+
 	posts := readPostsMetadata()
 	genPostPages(posts)
 	genIndexHtml(posts)
@@ -175,4 +181,65 @@ func readContacts() []Contact {
 	}
 
 	return contacts
+}
+
+func newPost() {
+	scanner := bufio.NewScanner((os.Stdin))
+
+	var post Post
+
+	fmt.Print("Title: ")
+	if hasMore := scanner.Scan(); !hasMore {
+		log.Fatal("Cannot read more lines.")
+	}
+	post.Title = scanner.Text()
+
+	fmt.Print("Name: ")
+	if hasMore := scanner.Scan(); !hasMore {
+		log.Fatal("Cannot read more lines.")
+	}
+	post.Name = scanner.Text()
+
+	fmt.Print("Is it a redirect? (y/n): ")
+	if hasMore := scanner.Scan(); !hasMore {
+		log.Fatal("Cannot read more lines.")
+	}
+	isRedirect := scanner.Text()
+
+	switch isRedirect {
+	case "y":
+		fmt.Print("Redirect to: ")
+		if hasMore := scanner.Scan(); !hasMore {
+			log.Fatal("Cannot read more lines.")
+		}
+		post.RedirectTo = scanner.Text()
+
+	case "n":
+		fmt.Println("Ok, no redirecting.")
+
+	default:
+		log.Fatal("Invalid answer; aborting.")
+	}
+
+	post.Date = time.Now().Format(postDateLayout)
+
+	posts := readPostsMetadata()
+	posts = append([]Post{post}, posts...)
+	postsData, err := json.MarshalIndent(posts, "", "    ")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := os.WriteFile("metadata.json", postsData, 0644); err != nil {
+		log.Fatal(err)
+	}
+
+	if isRedirect == "n" {
+		postContent := "<div class=\"introduction\">\n\n\n\n</div>\n"
+		if err := os.WriteFile(post.inputFilename(), []byte(postContent), 0644); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	fmt.Println("Done.")
 }
