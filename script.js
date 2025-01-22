dcf340a3df58b1fc618222d064f228c879ae525e
hirrolot <hirrolot@users.noreply.github.com>
2025-01-22 00:53:39 +0000

    deploy: 547d712f778cb70d8f8822d478ad161c4bc93483

---
+++ script.js
@@ -0,0 +1,26 @@
+window.addEventListener("load", function (event) {
+    genAuthorEmoji();
+});
+
+function genAuthorEmoji() {
+    const emojis = ["💋", "💄", "💅", "❤️", "👄", "👅", "👠", "👗", "🌹", "🌺", "🪷", "🌷", "💐", "🫒", "🥀", "🌼", "🌱", "🌳", "🌴", "🍁", "🍄", "🌹", "🥩", "🍎", "🍒", "🍓", "🍇", "🍉", "🍌", "🍫", "🍰", "🍭", "🍬", "🍧", "🍝", "🎈", "💕", "💖", "💘", "💞", "💓", "💝", "💗", "❤️‍🔥", "🫀", "🎀", "🎁", "🌈", "🦄", "🔮", "🎑", "🧸", "🐬", "🦔", "🦇", "🦨", "🦬", "🐌", "🐞", "🦋", "🌀", "🎎", "🪆", "🚩", "🛕", "⛪", "🏰", "🐚", "👁️", "🎼", "🪽🪽🪽", "🕊️", "🗽", "🪐", "🎅", "🎄", "☃️", "🌌", "🏜️", "🌉", "🌃", "🌆", "🌇", "🏙", "🎆", "🎑", "🪴", "⛲", "🪅", "🎨", "🍹", "⛩️", "🎭", "🥘"];
+
+    const randomEmoji = document.createElement("span");
+    randomEmoji.textContent = emojis[Math.floor(Math.random() * emojis.length)];
+    randomEmoji.innerHTML += "&nbsp;";
+
+    const blogLink = document.createElement("a");
+    blogLink.href = "..";
+    blogLink.textContent = "hirrolot";
+
+    const fancyAuthor = document.createElement("p");
+    fancyAuthor.className = "author";
+    fancyAuthor.appendChild(randomEmoji);
+    fancyAuthor.appendChild(blogLink);
+
+    const author = document.querySelector(".author");
+
+    if (author) {
+        author.replaceWith(fancyAuthor);
+    }
+}
