f0eded9b677a8bd60c8cd15bfe5edfb9b4cb00d5
hirrolot <hirrolot@gmail.com>
2026-08-31 21:28:18 +0100

    `--math-method=mathjax`

--- gen.go
+++ gen.go
@@ -109,7 +109,7 @@ func invokePandoc(post *Post) {
 		"--metadata", fmt.Sprintf("title=%s", post.Title),
 		"--metadata", fmt.Sprintf("date=%s", post.Date),
 		"--standalone",
-		"--mathjax",
+		"--math-method=mathjax",
 		"-H", "header.html",
 		"--table-of-contents",
 		"--citeproc",
